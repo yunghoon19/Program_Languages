5f5eebb25110ac666e454f60b7ed474dd80ce620
YOUNGHOON1010 <82190412+yunghoon19@users.noreply.github.com>
2022-04-20 21:56:18 +0900

    단일연결리스트
    구현중

--- Cpp/Data _Structure/Singly_Linked_List.cpp
+++ Cpp/Data _Structure/Singly_Linked_List.cpp
@@ -6,57 +6,59 @@
 
 using namespace std;
 
+template <typename T>
 class Node  // 리스트에 하나의 노드
 {
 private:
-    int value;      // 노드가 저장할 값
+    T value;      // 노드가 저장할 값
 
 public:
     Node* nextNode; // 다음 노드 주소
 
 public:
-    Node(int _n) : value(_n), nextNode(NULL) { cout << "Node Create!" << endl; }
+    Node(T _n) : value(_n), nextNode(NULL) { cout << "Node Create!" << endl; }
+    ~Node() { cout << "Node Destroy!" << endl; }
 
 public:
     int GetValue() { return value; }
 };
 
+template <typename T>
 class Linked_List    // 단일 연결 리스트
 {
 private:
     int listSize;       // 리스트의 크기
-    Node* rootNode;     // 리스트의 루트 노드
+    Node<T>* rootNode;     // 리스트의 루트 노드
 
 public:
     Linked_List() : rootNode(NULL), listSize(0) { cout << "Linked_List Create!" << endl; };
     ~Linked_List()
     {
+        cout << "Linked_List Destroy!" << endl;
         while (listSize--)
         {
-            Node* temp = rootNode->nextNode;
+            Node<T>* temp = rootNode->nextNode;
 
             delete rootNode;
             rootNode = temp;
-
-            //cout << "Node Destroy" << endl;
         }
     }
 
 public:
     // 리스트 맨 끝에 노드를 추가하는 함수
-    void AddInLastNode(int _value)  
+    void AddInLastNode(T _value)  
     {
         if (rootNode == NULL)
         {
-            rootNode = new Node(_value);
+            rootNode = new Node<T>(_value);
         }
         else
         {
-            Node* locationCheck = rootNode;
+            Node<T>* locationCheck = rootNode;
 
             while (locationCheck->nextNode != NULL) { locationCheck = locationCheck->nextNode; }
             
-            locationCheck->nextNode = new Node(_value);
+            locationCheck->nextNode = new Node<T>(_value);
         }
         listSize++;
     }
@@ -68,9 +70,9 @@ public:
     }
 
     // 리스트의 마지막 노드의 value 리턴
-    int GetLastNodeValue()
+    T GetLastNodeValue()
     {
-        Node* lastNodeCheck = rootNode;
+        Node<T>* lastNodeCheck = rootNode;
 
         while (lastNodeCheck->nextNode != NULL) { lastNodeCheck = lastNodeCheck->nextNode; }
         
@@ -80,7 +82,7 @@ public:
     // 연결리스트의 모든 원소 출력
     void PrintList()
     {
-        Node* currentNode = rootNode;
+        Node<T>* currentNode = rootNode;
 
         while (currentNode != NULL)
         {
@@ -88,11 +90,13 @@ public:
             currentNode = currentNode->nextNode;
         }
     }
+
+    // 리스트의 원하는 위치에 데이터 삽입
 };
 
 int main(void)
 {
-    Linked_List l1;
+    Linked_List<int> l1;
 
     l1.AddInLastNode(1);
     l1.AddInLastNode(2);
