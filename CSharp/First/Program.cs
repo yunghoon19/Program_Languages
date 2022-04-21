1c2277f7fe5a2073e7b5e0bc15d050f24175c14d
YOUNGHOON1010 <82190412+yunghoon19@users.noreply.github.com>
2022-04-22 01:16:12 +0900

    CSharp
    배움은 즐거워 C# 입문

---
+++ CSharp/First/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint ep = new IPEndPoint(IPAddress.Any, 7000);
+            socket.Bind(ep);
+
+            socket.Listen(10);
+
+            Socket clientSocket = socket.Accept();
+
+            byte[] buff = new byte [8192];
+            while (!Console.KeyAvailable)
+            {
+                int n = clientSocket.Receive(buff);
+
+                string data = Encoding.UTF8.GetString(buff, 0, n);
+                Console.WriteLine(data);
+
+                clientSocket.Send(buff, 0, n, SocketFlags.None);
+            }
+
+            clientSocket.Close();
+            socket.Close();
+        }
+    }
+}
